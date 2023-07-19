8cf917f9c6825bcf612643d6f39319dcc6219b37
Xannosz <kiss.andras.zoltan@gmail.com>
2023-07-19 21:55:39 +0200

    Add diesel locomotive model

---
+++ bbmodel/java/diesel_locomotive.java
@@ -0,0 +1,45 @@
+// Made with Blockbench 4.7.4
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+
+public class electric_locomotive<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "electric_locomotive"), "main");
+	private final ModelPart body;
+
+	public electric_locomotive(ModelPart root) {
+		this.body = root.getChild("body");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition bottom = body.addOrReplaceChild("bottom", CubeListBuilder.create().texOffs(0, 0).addBox(-11.0F, 3.0F, -8.0F, 22.0F, 2.0F, 16.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 0).addBox(-10.5F, -5.0F, -2.0F, 1.0F, 4.0F, 4.0F, new CubeDeformation(0.0F))
+		.texOffs(60, 0).addBox(-0.5F, -9.0F, -1.5F, 3.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition top = body.addOrReplaceChild("top", CubeListBuilder.create().texOffs(32, 30).addBox(-10.0F, -7.0F, -7.0F, 7.0F, 10.0F, 14.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 18).addBox(-3.0F, -7.5F, -7.5F, 8.0F, 11.0F, 15.0F, new CubeDeformation(0.0F))
+		.texOffs(0, 44).addBox(4.5F, -7.0F, -7.0F, 6.0F, 10.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition main = body.addOrReplaceChild("main", CubeListBuilder.create().texOffs(7, 9).addBox(-11.0F, -4.5F, -1.5F, 1.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
+		.texOffs(10, 2).addBox(10.0F, -0.5F, -4.5F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(6, 0).addBox(10.0F, -0.5F, 2.5F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 128, 128);
+	}
+
+	@Override
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+}
